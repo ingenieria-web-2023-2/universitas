6eab0f556a02c9f2bdbd5ceed9bb3c36a6b4706d
Oscar Mesa <oscarmesa.elpoli@gmail.com>
2023-11-15 20:27:17 -0500

    implementacion de bootstrap

--- media/js/mi-api.js
+++ media/js/mi-api.js
@@ -1,6 +1,30 @@
-fetch('https://pokeapi.co/api/v2/type')
+const promesaApiPokemon = fetch('https://pokeapi.co/api/v2/type');
+
+  promesaApiPokemon
   .then(function(response) {
     console.log(response);
     return response.json();
   })
-  .then(data => console.log(data));
+  .then(data => {
+    let bodyTableHtml = "";
+    const resultados = data.results;
+    resultados.forEach(element => {
+      bodyTableHtml += `<tr>
+                            <td>${element.name}</td>
+                            <td><a href="${element.url}">${element.url}</a></td>
+                        </tr>`;      
+    });
+    document.querySelector("#tbl_pokemon tbody").innerHTML = bodyTableHtml;
+  });
+
+
+  const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("Se presentó un error");
+    }, 300);
+  });
+
+  myPromise
+  .then(respusta => console.log("entro por el then ",respusta))
+  .catch(error => console.log("entro por el catch ",error))
+  .finally(() => console.log("Se ejecuta siempre"));
