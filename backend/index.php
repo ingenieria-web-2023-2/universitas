a2576204630af4d4a6a01c8e3218b798a40fcccf
Oscar Mesa <oscarmesa.elpoli@gmail.com>
2023-11-24 19:35:32 -0500

    cambios en local

---
+++ backend/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Backend</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tu Plantilla</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        /* Estilos personalizados aquí */
+    </style>
+</head>
+<body>
+    <header>
+        <!-- Encabezado aquí -->
+    </header>
+
+    <main>
+        <p>Hello Backend!</p>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Documento</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Correo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>123456789</td>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>johndoe@example.com</td>
+                </tr>
+                <tr>
+                    <td>987654321</td>
+                    <td>Jane</td>
+                    <td>Smith</td>
+                    <td>janesmith@example.com</td>
+                </tr>
+            </tbody>
+        </table>
+    
+  </main>
+
+  <footer>
+    <!-- Pie de página aquí -->
+  </footer>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script>
+    // Scripts personalizados aquí
+  </script>
+</body>
+</html>
---
+++ backend/suma.php
@@ -0,0 +1,11 @@
+<?php 
+    echo 'Hola poli!!!!';
+    echo "Hola clase";
+
+    $numero1 = 5;
+    $numero2 = 10;
+    $suma = $numero1 + $numero2;
+
+    echo "<br/><br/><br/>";
+    echo "La suma de $numero1 + $numero2 es: $suma";
+?>
