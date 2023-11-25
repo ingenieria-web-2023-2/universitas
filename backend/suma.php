5fc5d8bf4c408bf886dff1fd20e86c0926656146
Oscar Mesa <oscarmesa.elpoli@gmail.com>
2023-11-24 21:02:45 -0500

    cambios en local

--- backend/suma.php
+++ backend/suma.php
@@ -8,4 +8,44 @@
 
     echo "<br/><br/><br/>";
     echo "La suma de $numero1 + $numero2 es: $suma";
+
+    //echo phpinfo();
+    echo gettype($suma);
+
+    /** 
+     * Arreglos 
+     * */
+    $arreglo = array(1,2,3,true,5,6,7,"Hola",9,array(2.3,4.5,6.7));
+    array_push($arreglo, "Julian", "oscar", "daniel"); 
+    echo "<pre>";
+    print_r($arreglo);
+    var_dump($arreglo);
+    echo $arreglo[9][1];
+    
+
+    $semanas = [
+        "julian1" => "Miercoles",
+        "Martes",
+        "Lunes",
+    ];
+    asort($semanas);
+    print_r($semanas);
+
+
+    $numeros = array_fill(1000,2000, 6);
+
+    //var_dump($numeros);
+
+    /**
+     * $_SERVER
+     */
+
+    print_r($_SERVER);
+
+
+    /**
+     * $_GET
+     */
+    print_r($_GET);
+    echo "</pre>";
 ?>
